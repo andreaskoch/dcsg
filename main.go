5e948f8263d4f6d7236753b19a472f4ee63c0b15
Dennis Hermsmeier <dennis.hermsmeier@gmail.com>
2018-11-01 13:52:37 +0100

    remove default value and add explanation

--- main.go
+++ main.go
@@ -16,8 +16,8 @@ var (
 
 	// install
 	installCommand                     = app.Command("install", "Register a systemd service for the given docker-compose file")
-	installDockerComposeFile           = installCommand.Arg("docker-compose-file", "A docker-compose file").Default("docker-compose.yml").ExistingFile()
-	installDockerComposeExtensionFiles = installCommand.Flag("extend-compose", "Additional docker-compose file").Short('e').ExistingFiles()
+	installDockerComposeFile           = installCommand.Arg("docker-compose-file", "A docker-compose file").ExistingFile()
+	installDockerComposeExtensionFiles = installCommand.Flag("extend-compose", "Additional docker-compose file (one file per flag)").Short('e').ExistingFiles()
 	installProjectName                 = installCommand.Arg("project-name", "The project name of the docker-compose project").String()
 	installDontPull                    = installCommand.Flag("no-pull", "Whether you want to pull the image before startup or not").Default("false").Bool()
 
